6af84e7c4f8ea6f4e6d3ca416de97819161da055
Kelve T. Henrique <kthenrique@pm.me>
2025-10-24 00:41:20 +0200

    Add test SurvivalBy3DoomedNeighbours

--- game_of_life/no_constraints/test/game_of_life.cpp
+++ game_of_life/no_constraints/test/game_of_life.cpp
@@ -25,3 +25,16 @@ TEST(GoL, SurvivalBy2DoomedNeighbours) {
 
   EXPECT_EQ(expected_generation, next_generation);
 }
+
+TEST(GoL, SurvivalBy3DoomedNeighbours) {
+
+  Cells initial_cells = {{0, 0}, {1, 1}, {0, 2}, {2, 2}};
+  Cells next_cells = {{1, 1}};
+
+  Generation generation{std::move(initial_cells)};
+
+  auto next_generation = generation.next();
+  Generation expected_generation{next_cells};
+
+  EXPECT_EQ(expected_generation, next_generation);
+}
