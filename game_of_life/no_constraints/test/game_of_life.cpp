fca8a2bc62b58fc4e9831cdf5407591de9207fa5
Kelve T. Henrique <kthenrique@pm.me>
2025-10-24 00:55:09 +0200

    Add test TooManyNeighboursIsFatal

--- game_of_life/no_constraints/test/game_of_life.cpp
+++ game_of_life/no_constraints/test/game_of_life.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
 #include <inc/generation.hpp>
 
+TEST(GoL, NoNeighboursMeansExtinction) {
+
+  Cells initial_cells = {{0, 0}, {0, 2}, {2, 0}, {2, 2}};
+
+  Generation generation{std::move(initial_cells)};
+
+  auto next_generation = generation.next();
+  Generation expected_generation;
+
+  EXPECT_EQ(expected_generation, next_generation);
+}
+
 TEST(GoL, UnderPopulationResultsInExtinction) {
 
   Cells initial_cells = {{1, 1}, {2, 0}};
@@ -30,6 +42,19 @@ TEST(GoL, SurvivalBy3DoomedNeighbours) {
 
   Cells initial_cells = {{0, 0}, {1, 1}, {0, 2}, {2, 2}};
   Cells next_cells = {{1, 1}};
+
+  Generation generation{std::move(initial_cells)};
+
+  auto next_generation = generation.next();
+  Generation expected_generation{next_cells};
+
+  EXPECT_EQ(expected_generation, next_generation);
+}
+
+TEST(GoL, TooManyNeighboursIsFatal) {
+
+  Cells initial_cells = {{0, 0}, {1, 0}, {0, 1}, {2, 1}, {1, 2}};
+  Cells next_cells = {{0, 0}, {2, 1}, {1, 2}};
 
   Generation generation{std::move(initial_cells)};
 
