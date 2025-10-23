67963b5dd53a037fabe2e03cb61099097fa222e5
Kelve T. Henrique <kthenrique@pm.me>
2025-10-23 23:14:38 +0200

    Add test UnderPopulationResultsInExtinction

--- game_of_life/no_constraints/test/game_of_life.cpp
+++ game_of_life/no_constraints/test/game_of_life.cpp
@@ -1,3 +1,14 @@
 #include <gtest/gtest.h>
+#include <inc/generation.hpp>
 
-TEST(GoL, UnderPopulationResultsInExtinction) { SUCCEED(); }
+TEST(GoL, UnderPopulationResultsInExtinction) {
+
+  Cells initial_cells = {{1, 1}, {2, 0}};
+
+  Generation generation{initial_cells};
+
+  auto next_generation = generation.next();
+  Generation expected_generation;
+
+  EXPECT_EQ(expected_generation, next_generation);
+}
