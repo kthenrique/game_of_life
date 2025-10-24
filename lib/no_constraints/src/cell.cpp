ecf45e562584ba0d5f3ddf8080c8b7842ee484dc
Kelve T. Henrique <kthenrique@pm.me>
2025-10-24 21:13:35 +0200

    Accept cells from the entire two dimensional space

--- lib/no_constraints/src/cell.cpp
+++ lib/no_constraints/src/cell.cpp
@@ -1,31 +1,8 @@
 #include <cell.hpp>
 
-// TODO: Remove constraint on first quadrant and allow negative cells
 Cells Cell::neighbours() const {
-  Cells neighbours = {{x, y + 1}, {x + 1, y + 1}, {x + 1, y}};
-  auto const at_origin = x == 0 && 0 == y;
-  auto const not_adjacent_to_any_axis = x != 0 && y != 0;
-  if (at_origin) {
-    return neighbours;
-  }
-
-  if (not_adjacent_to_any_axis) {
-    neighbours.insert({{x - 1, y - 1},
-                       {x - 1, y},
-                       {x - 1, y + 1},
-                       {x + 1, y - 1},
-                       {x, y - 1}});
-    return neighbours;
-  }
-
-  if (y == 0) {
-    neighbours.insert({{x - 1, y}, {x - 1, y + 1}});
-  }
-
-  if (x == 0) {
-    neighbours.insert({{x + 1, y - 1}, {x, y - 1}});
-  }
-
+  Cells neighbours = {{x - 1, y - 1}, {x - 1, y}, {x - 1, y + 1}, {x, y + 1},
+                      {x + 1, y + 1}, {x + 1, y}, {x + 1, y - 1}, {x, y - 1}};
   return neighbours;
 }
 
---
+++ lib/no_lists/src/generation.cpp
@@ -0,0 +1,4 @@
+#include <generation.hpp>
+#include <iostream>
+
+Generation &Generation::next() { return *this; }
