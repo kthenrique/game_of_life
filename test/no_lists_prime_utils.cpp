91dd41137a51a4a082c09c2e42581e4b6c9197e4
Kelve T. Henrique <kthenrique@pm.me>
2025-10-31 16:47:39 +0100

    Add tests for get_nr_factors utility function

--- test/no_lists_prime_utils.cpp
+++ test/no_lists_prime_utils.cpp
@@ -3,6 +3,14 @@
 #include <prime_utils.hpp>
 #include <utility>
 
+TEST(AssertionFailed, CellOutsideEnclosingGrid) {
+  EXPECT_DEATH(get_prime_from_cell({5, 5}, {1, 1}), ",*");
+}
+
+TEST(AssertionFailed, PrimeOutsideEnclosingGrid) {
+  EXPECT_DEATH(get_cell_from_prime(17, {1, 1}), ",*");
+}
+
 struct Grid_cell_prime {
   using Expected_config = std::vector<std::pair<Cell, int>>;
   Cell enclosing_grid;
@@ -11,6 +19,19 @@ struct Grid_cell_prime {
 
 class PrimeParameterizedTests : public testing::TestWithParam<Grid_cell_prime> {
 };
+
+TEST_P(PrimeParameterizedTests, GetPrimeAndCell) {
+  auto const enclosing_grid = GetParam().enclosing_grid;
+  auto const expectations = GetParam().expectations;
+
+  for (auto const &exp : expectations) {
+    auto const &cell = exp.first;
+    int const &prime = exp.second;
+
+    EXPECT_EQ(prime, get_prime_from_cell(cell, enclosing_grid));
+    EXPECT_EQ(cell, get_cell_from_prime(prime, enclosing_grid));
+  }
+}
 
 INSTANTIATE_TEST_SUITE_P(
     PrimeUtils, PrimeParameterizedTests,
@@ -41,23 +62,36 @@ INSTANTIATE_TEST_SUITE_P(
 
         ));
 
-TEST_P(PrimeParameterizedTests, GetPrimeWhenGridIsMinimum) {
-  auto enclosing_grid = GetParam().enclosing_grid;
-  auto expectations = GetParam().expectations;
-
-  for (auto const &exp : expectations) {
-    auto const &cell = exp.first;
-    int const &prime = exp.second;
-
-    EXPECT_EQ(prime, get_prime_from_cell(cell, enclosing_grid));
-    EXPECT_EQ(cell, get_cell_from_prime(prime, enclosing_grid));
-  }
+TEST(NumberOfPrimeFactors, ZeroFactors) {
+  EXPECT_EQ(0, get_nr_factors(0));
+  EXPECT_EQ(0, get_nr_factors(1));
 }
 
-TEST(AssertionFailed, CellOutsideEnclosingGrid) {
-  EXPECT_DEATH(get_prime_from_cell({5, 5}, {1, 1}), ",*");
+struct Factorization {
+  std::size_t product;
+  std::size_t nr_factors;
+};
+
+class NumberOfPrimeFactors : public ::testing::TestWithParam<Factorization> {};
+
+TEST_P(NumberOfPrimeFactors, GetNrOfFactors) {
+  auto const product = GetParam().product;
+  auto const expected_nr_factors = GetParam().nr_factors;
+
+  EXPECT_EQ(expected_nr_factors, get_nr_factors(product));
+  EXPECT_EQ(expected_nr_factors, get_nr_factors(product));
 }
 
-TEST(AssertionFailed, PrimeOutsideEnclosingGrid) {
-  EXPECT_DEATH(get_cell_from_prime(17, {1, 1}), ",*");
-}
+INSTANTIATE_TEST_SUITE_P(PrimeFactors, NumberOfPrimeFactors,
+                         ::testing::Values(
+                             // Zero factors:
+                             Factorization{0, 0}, Factorization{1, 0},
+                             // One factor:
+                             Factorization{2, 1}, Factorization{7, 1},
+                             Factorization{23, 1},
+                             // Multiple different factors:
+                             Factorization{21, 2}, Factorization{110, 3},
+                             Factorization{10465, 4},
+                             // Multiple equal factors:
+                             Factorization{49, 2}, Factorization{1331, 3},
+                             Factorization{279841, 4}));
