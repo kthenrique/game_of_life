d7f3520d79a23e8794c515aad8faba50a1f44b97
Kelve T. Henrique <kthenrique@pm.me>
2025-10-31 17:21:46 +0100

    Add Cells interface for impl with constraint no_lists

---
+++ lib/no_lists/inc/cells.hpp
@@ -0,0 +1,47 @@
+#ifndef CELLS_HPP
+#define CELLS_HPP
+
+#include <cell.hpp>
+#include <prime_utils.hpp>
+#include <unordered_set>
+
+class Cells {
+public:
+  struct Quadrant {
+    Cell enclosing_grid{0, 0};
+    std::size_t product{1};
+
+    constexpr bool operator==(const Quadrant &) const = default;
+  };
+
+  // Convert default interface Cells to this Cells type
+  // rationale: reuse already written tests
+  Cells(std::unordered_set<Cell> const &cells);
+  Cells() = default;
+
+  struct iterator {
+    struct Quadrant_it : public Quadrant {
+      std::size_t nr_cells{0};
+      std::size_t current_cell_index{1};
+      constexpr bool operator==(const Quadrant_it &) const = default;
+    } first, second, third, fourth;
+
+    Cell operator*() const;
+    iterator &operator++();
+    bool operator==(iterator const &other) const;
+  };
+
+  iterator begin() const;
+  iterator end() const;
+
+  bool contains(Cell cell);
+  std::pair<Cell, bool> insert(Cell cell);
+  void erase(Cell cell);
+
+  bool operator==(Cells const &other) const;
+
+private:
+  Quadrant m_first, m_second, m_third, m_fourth;
+  Quadrant &determine_cell_quadrant(Cell cell);
+};
+#endif
